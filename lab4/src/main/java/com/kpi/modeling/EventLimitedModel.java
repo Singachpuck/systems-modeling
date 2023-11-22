8d274242454179c39c59d99a8d3651fb95e9f20f
Singachpuck <singachpucktv@gmail.com>
2023-11-22 16:23:44 +0100

    Lab 4: Improve tests - add second model - no printing

--- lab4/src/main/java/com/kpi/modeling/EventLimitedModel.java
+++ lab4/src/main/java/com/kpi/modeling/EventLimitedModel.java
@@ -2,6 +2,14 @@ package com.kpi.modeling;
 
 public class EventLimitedModel extends Model {
 
+    @Override
+    protected void printInfo() {
+    }
+
+    @Override
+    protected void printStatistic() {
+    }
+
     public void simulateWithLimitedEvents(int limit) {
         this.init();
         while (numProcess <= limit) {
--- lab4/src/test/java/com/kpi/modeling/PerformanceModelTest.java
+++ lab4/src/test/java/com/kpi/modeling/PerformanceModelTest.java
@@ -15,10 +15,10 @@ public class PerformanceModelTest {
 
     static Stream<Arguments> range() {
         final int nStart = 100,
-                nEnd = 1000,
+                nEnd = 1500,
                 nStep = 100;
         final List<Arguments> arguments = new ArrayList<>();
-        for (int i = nStart; i < nEnd; i+= nStep) {
+        for (int i = nStart; i <= nEnd; i+= nStep) {
             arguments.add(Arguments.of(i));
         }
         return arguments.stream();
@@ -58,6 +58,61 @@ public class PerformanceModelTest {
         model.simulateWithLimitedEvents(n);
         long after = System.nanoTime();
         System.out.println("Processed: " + model.numProcess);
-        System.out.println("Elapsed time: " + (after - before));
+        System.out.println("Elapsed time: " + (after - before) / 1_000_000_000D);
+    }
+
+    @ParameterizedTest
+    @MethodSource("range")
+    void parallelChainPerformance(int n) {
+        final EventLimitedModel model = new EventLimitedModel();
+        final List<BaseItem> schema = new ArrayList<>();
+
+        final Distribution createDist = new Distribution(Distribution.DistEnum.EXP, 5.0);
+        final Create c = new Create(model, "Create", createDist, BaseItem.Mode.PROB);
+        schema.add(c);
+
+        BaseItem parent1 = null;
+        BaseItem parent2 = null;
+        boolean first = true;
+        for (int i = 0; i < n; i+=2) {
+            final Distribution childDist = new Distribution(Distribution.DistEnum.NORMAL, 10.0, 5.0);
+            final MultichannelProcess next1 = new MultichannelProcess(model, "Process " + i, -1, BaseItem.Mode.PROB);
+            final Process child11 = new Process(model, "Process " + i + " child 1", childDist, -1, BaseItem.Mode.PROB);
+            final Process child12 = new Process(model, "Process " + i + " child 2", childDist, -1, BaseItem.Mode.PROB);
+            next1.addChannel(child11);
+            next1.addChannel(child12);
+            schema.add(next1);
+
+            final MultichannelProcess next2 = new MultichannelProcess(model, "Process " + i, -1, BaseItem.Mode.PROB);
+            final Process child21 = new Process(model, "Process " + i + " child 1", childDist, -1, BaseItem.Mode.PROB);
+            final Process child22 = new Process(model, "Process " + i + " child 2", childDist, -1, BaseItem.Mode.PROB);
+            next2.addChannel(child21);
+            next2.addChannel(child22);
+            schema.add(next2);
+
+            if (first) {
+                c.addNext(next1, 0.5);
+                c.addNext(next2, 0.5);
+                first = false;
+            } else {
+                parent1.addNext(next1);
+                parent2.addNext(next2);
+            }
+            parent1 = next1;
+            parent2 = next2;
+        }
+
+        final Destroy d = new Destroy(model, "Destroy");
+        schema.add(d);
+        parent1.addNext(d);
+        parent2.addNext(d);
+
+        model.setSchema(schema);
+
+        long before = System.nanoTime();
+        model.simulateWithLimitedEvents(n);
+        long after = System.nanoTime();
+        System.out.println("Processed: " + model.numProcess);
+        System.out.println("Elapsed time: " + (after - before) / 1_000_000_000D);
     }
 }
